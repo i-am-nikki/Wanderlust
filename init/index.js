b1bc263fbbdc6adfa755c8f4f10d92eafbcbf9cc
i-am-nikki <nikkimalik355@gmail.com>
2023-12-30 18:45:13 +0530

    initial Wanderlust

---
+++ init/index.js
@@ -0,0 +1,28 @@
+//Logic for initialization 
+
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({}); //if data exists, delete them first
+  initData.data = initData.data.map((obj) => ({...obj, owner: "658250bcec4db9a4172a916f"}));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
